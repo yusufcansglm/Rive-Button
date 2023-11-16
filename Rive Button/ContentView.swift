95295c67ba77d5080928d3c09b6323a134657cf6
Yusuf Can Sağlam <yusufcan.sglm@icloud.com>
2023-11-16 15:29:19 +0300

    Initial Commit

---
+++ Rive Button/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rive Button
+//
+//  Created by Yusuf Can Sağlam on 16.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rive Button/Rive_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rive_ButtonApp.swift
+//  Rive Button
+//
+//  Created by Yusuf Can Sağlam on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Rive_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
